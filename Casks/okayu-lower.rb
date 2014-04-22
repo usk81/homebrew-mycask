982330d142b32d42e103d328831614fec2aa12d0
Yusuke Komatsu <tienlen042@gmail.com>
2014-04-23 03:05:42 +0900

    ADD: create cask files for okayu.app

---
+++ Casks/okayu-lower.rb
@@ -0,0 +1,8 @@
+# Okayu for Under 10.7
+class Okayu < Cask
+  url 'http://kentaroimai.com/products/okayu/Okayu-0.5.01.dmg'
+  homepage 'http://kentaroimai.com/projects/okayu'
+  version '0.5.01'
+  sha256 '65befbbf4a52642cf879fb3b4aaa62e96948e377f68569c630c57a047ef44e38'
+  link 'Okayu.app'
+end
---
+++ Casks/okayu.rb
@@ -0,0 +1,8 @@
+# Okayu for ver.10.8+
+class Okayu < Cask
+  url 'http://kentaroimai.com/products/okayu/Okayu-0.8.01.dmg'
+  homepage 'http://kentaroimai.com/projects/okayu'
+  version '0.8.01'
+  sha256 'c09b705ae054158615fc6073e5da6afc46f3477c3cce6fb359df89d4ff659986'
+  link 'Okayu.app'
+end
