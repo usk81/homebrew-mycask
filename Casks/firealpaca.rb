e2449f5fea7b0150ca5eccb6487e05cd0e1b3237
Yusuke Komatsu <tienlen042@gmail.com>
2014-05-22 18:07:46 +0900

    Add FireAlpaca's cask

---
+++ Casks/firealpaca.rb
@@ -0,0 +1,7 @@
+class Firealpaca < Cask
+  url 'http://firealpaca.com/download.php?os=mac&key=17813449013210197561d4f66c5aca8c'
+  homepage 'http://firealpaca.com/'
+  version '1.0.51'
+  sha256 '67a9cd14fece5e83d8b0d65454acab5cf1a2fdda7b830de116a92f8d0754cf2f'
+  link 'FireAlpaca.app'
+end
