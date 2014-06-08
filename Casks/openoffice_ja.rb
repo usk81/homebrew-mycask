3c50de7eaf22f6c666967671882a72f9af13ddb9
Yusuke Komatsu <tienlen042@gmail.com>
2014-06-09 03:29:53 +0900

    ADD openoffice_ja

---
+++ Casks/openoffice_ja.rb
@@ -0,0 +1,7 @@
+class OpenofficeJa < Cask
+  url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.0/binaries/ja/Apache_OpenOffice_4.1.0_MacOS_x86-64_install_ja.dmg'
+  homepage 'http://www.openoffice.org/'
+  version '4.1.0'
+  sha256 'd989459dca99f1e487f43c0664e0a07626adebe27b845fa0baee557a05f11b90'
+  link 'OpenOffice.app'
+end
